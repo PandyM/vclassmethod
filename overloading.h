9f529c02c441de4d2e31659fd3398dd5ae7e517d
Pandy muhamad <33719236+PandyM@users.noreply.github.com>
2020-04-30 14:44:20 +0700

    Create overloading.h

---
+++ overloading.h
@@ -0,0 +1,16 @@
+#ifndef VERLOADING_H
+#define VERLOADING_H
+
+
+class overloading
+{
+    public:
+        overloading();
+        virtual ~overloading();
+
+    protected:
+
+    private:
+};
+
+#endif // OVERLOADING_H
